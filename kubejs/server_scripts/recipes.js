6731fc6080c40efcf771734605e4c8a6d7d94097
ur-alas <al+github2@drye.ga>
2025-11-15 12:50:28 -0800

    Added KubeJS configs, removed Immersive Portals & Infinity

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,31 @@
+ServerEvents.recipes(event => {
+   event.remove({ mod: 'yttr' }); //remove yttr recipes
+   event.remove({ output: '#yttr:antler_potion' }) //not working for some godforsaken reason
+   //create recipes
+   //gadolinite -> crushed yttrium recipes
+   event.recipes.create.crushing(['kubejs:crushed_raw_yttrium', Item.of('create:crushed_raw_iron').withChance(0.25), Item.of('create:experience_nugget').withChance(0.75)], 'yttr:raw_gadolinite')
+   event.recipes.create.crushing(['kubejs:crushed_raw_yttrium', Item.of('kubejs:crushed_raw_yttrium').withChance(0.75), Item.of('create:crushed_raw_iron').withChance(0.50), Item.of('create:experience_nugget').withChance(0.75), Item.of('minecraft:cobblestone').withChance(0.12)], 'yttr:gadolinite')
+   event.recipes.create.crushing(['2x kubejs:crushed_raw_yttrium', Item.of('kubejs:crushed_raw_yttrium').withChance(0.25), Item.of('create:crushed_raw_iron').withChance(0.50), Item.of('create:experience_nugget').withChance(0.75), Item.of('minecraft:deepslate').withChance(0.12)], 'yttr:deepslate_gadolinite')
+   event.recipes.create.crushing(['9x kubejs:crushed_raw_yttrium', Item.of('9x create:crushed_raw_iron').withChance(0.50), Item.of('9x create:experience_nugget').withChance(0.75)], 'yttr:raw_gadolinite_block')
+
+
+   //crushed yttrium -> ingots recipes
+   event.recipes.create.splashing(['9x yttr:yttrium_nugget', Item.of('4x minecraft:iron_nugget').withChance(0.75)], 'kubejs:crushed_raw_yttrium')
+   event.smelting('yttr:yttrium_ingot', 'kubejs:crushed_raw_yttrium').xp(0.7).cookingTime(200)
+   event.blasting('yttr:yttrium_ingot', 'kubejs:crushed_raw_yttrium').xp(0.7).cookingTime(100)
+
+   //yttrium parts
+   event.recipes.create.pressing('kubejs:yttrium_plate', 'yttr:yttrium_ingot')
+   event.custom({
+       type: 'createaddition:rolling',
+       ingredients: [{ item: 'yttr:yttrium_ingot' }],
+       result: { item: 'kubejs:yttrium_rod', count: 2 },
+})
+
+
+
+
+
+});
+//ctrl alt c for fancymenu bar
+// disabled all logistics blocks from yttr
---
+++ kubejs/startup_scripts/customItems.js
@@ -0,0 +1,7 @@
+StartupEvents.registry('item', event => {
+   event.create('crushed_raw_yttrium').displayName('Crushed Raw Yttrium'); //using osmium texture
+   event.create('yttrium_rod').displayName('Yttrium Rod'); //needs texture
+   event.create('yttrium_plate').displayName('Yttrium Plate'); //needs texture
+
+
+});
