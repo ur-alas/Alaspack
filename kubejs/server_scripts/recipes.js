2a458899fa4e770320181c8a92319db90417f2da
ur-alas <al+github2@drye.ga>
2025-11-15 18:09:58 -0800

    Multiple Rifle Cartridges

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -65,6 +65,121 @@ ServerEvents.recipes((event) => {
     ingredients: [{ item: "yttr:yttrium_ingot" }],
     result: { item: "kubejs:yttrium_rod", count: 2 },
   });
+  // bedrock breaker spike = diamond + yttrium + lava + superheated
+  event.recipes.create
+    .mixing("kubejs:hardened_diamond_spike", [
+      "minecraft:diamond",
+      "2x yttr:yttrium_ingot",
+      Fluid.of("minecraft:lava"),
+    ])
+    .superheated();
+  event.recipes.create.mechanical_crafting(
+    "yttr:bedrock_smasher",
+    ["     ", " AAA ", "AACAA", " BCB ", "  B  "],
+    {
+      A: "minecraft:obsidian",
+      B: "kubejs:hardened_diamond_spike",
+      C: "kubejs:yttrium_rod",
+    }
+  );
+
+  event.recipes.create.deploying(
+    Item.of("yttr:ammo_can", { Shots: 1024, Mode: "DAMAGE" }),
+    ["kubejs:yttrium_bolts"]
+  );
+
+  const cartridgeTransitional = "kubejs:incomplete_cartridge"; // Making a constant to store the transitional item makes the code more readable
+
+  event.recipes.create.mechanical_crafting(
+    "4x kubejs:yttrium_beam",
+    [" A ", " B ", " B "],
+    {
+      A: "kubejs:yttrium_rod",
+      B: "minecraft:blaze_powder",
+    }
+  );
+
+  event.recipes.create.pressing(
+    "kubejs:yttrium_plate",
+    cartridgeTransitional
+  );
+
+  event.recipes.create
+    .sequenced_assembly(
+      // Outputs:
+      [
+        Item.of("kubejs:beam_cartridge"), // Main output, will appear in JEI as the result
+      ],
+      // Input:
+      cartridgeTransitional,
+      // Sequence:
+      [
+        // The transitional item is a constant, that is 'kubejs:incomplete_spore_blossom' and is used during the intermediate stages of the assembly.
+        // Like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item.
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:yttrium_beam",
+        ]),
+      ]
+    )
+    .transitionalItem(cartridgeTransitional) // Set the transitional item
+    .loops(8); // Set the number of loops
+
+  event.recipes.create.pressing(
+    Item.of("yttr:ammo_can", { Shots: 1024, Mode: "DAMAGE" }),
+    ["kubejs:beam_cartridge", "yttr:empty_ammo_can"]
+  );
+
+  event.recipes.create.deploying(
+    "kubejs:explode_yttrium_beam",
+    ["minecraft:tnt", "kubejs:yttrium_beam"]
+  );
+
+
+  event.recipes.create
+    .sequenced_assembly( // Set the transitional item
+      [
+        Item.of("kubejs:explode_cartridge"),
+      ],
+      cartridgeTransitional,
+      [
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:explode_yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:explode_yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:explode_yttrium_beam",
+        ]),
+        event.recipes.create.deploying(cartridgeTransitional, [
+          cartridgeTransitional,
+          "kubejs:explode_yttrium_beam",
+        ]),
+      ]
+    )
+    .transitionalItem(cartridgeTransitional)
+    .loops(8);
+
+  event.recipes.create.pressing(
+    Item.of("yttr:ammo_can", { Shots: 1024, Mode: "EXPLODE"}),
+    ["kubejs:explode_cartridge", "yttr:empty_ammo_can"]
+  );
 
   //rifle canister nbt:
   //Shots: 0-1024
