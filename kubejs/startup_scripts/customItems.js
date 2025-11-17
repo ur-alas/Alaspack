5072b845f01b27084ed983da7d1294583f82fdf4
ur-alas <al+github2@drye.ga>
2025-11-17 14:15:10 -0800

    Revert new namespace. it breaks things.

--- kubejs/startup_scripts/customItems.js
+++ kubejs/startup_scripts/customItems.js
@@ -1,21 +1,21 @@
 StartupEvents.registry("item", (event) => {
-  event.create("yttrCreate:crushed_raw_yttrium").displayName("Crushed Raw Yttrium"); //using osmium texture
-  event.create("yttrCreate:yttrium_rod").displayName("Yttrium Rod"); //needs texture
-  event.create("yttrCreate:yttrium_plate").displayName("Yttrium Plate"); //needs texture
-  event.create("yttrCreate:hardened_diamond_spike").displayName("Hardened Diamond Spike"); //needs texture
-  event.create("yttrCreate:yttrium_beam").displayName("Yttrium Beam"); //needs texture
-  event.create("yttrCreate:explode_yttrium_beam").displayName("Explosive Yttrium Beam"); //needs texture
-  event.create("yttrCreate:fire_yttrium_beam").displayName("Flaming Yttrium Beam");//needs texture
-  event.create("yttrCreate:teleport_yttrium_beam").displayName("Teleport Yttrium Beam");//needs texture
-  event.create("yttrCreate:illumination_yttrium_beam").displayName("Light Yttrium Beam");//needs texture
-  event.create("yttrCreate:incomplete_cartridge").displayName("Incomplete Cartridge");//needs texture
-  event.create("yttrCreate:beam_cartridge").displayName("Beam Cartridge");//needs texture
-  event.create("yttrCreate:explode_cartridge").displayName("Explosive Cartridge");//needs texture
-  event.create("yttrCreate:fire_cartridge").displayName("Fire Cartridge");//needs texture
-  event.create("yttrCreate:teleport_cartridge").displayName("Teleport Cartridge");//needs texture
-  event.create("yttrCreate:illumination_cartridge").displayName("Light Cartridge");//needs texture
-  event.create("yttrCreate:uncured_void_cartridge").displayName("Uncured Void Cartridge");//needs texture
-  event.create("yttrCreate:void_cartridge").displayName("Void Cartridge");//needs texture
+  event.create("crushed_raw_yttrium").displayName("Crushed Raw Yttrium"); //using osmium texture
+  event.create("yttrium_rod").displayName("Yttrium Rod"); //needs texture
+  event.create("yttrium_plate").displayName("Yttrium Plate"); //needs texture
+  event.create("hardened_diamond_spike").displayName("Hardened Diamond Spike"); //needs texture
+  event.create("yttrium_beam").displayName("Yttrium Beam"); //needs texture
+  event.create("explode_yttrium_beam").displayName("Explosive Yttrium Beam"); //needs texture
+  event.create("fire_yttrium_beam").displayName("Flaming Yttrium Beam");//needs texture
+  event.create("teleport_yttrium_beam").displayName("Teleport Yttrium Beam");//needs texture
+  event.create("illumination_yttrium_beam").displayName("Light Yttrium Beam");//needs texture
+  event.create("incomplete_cartridge").displayName("Incomplete Cartridge");//needs texture
+  event.create("beam_cartridge").displayName("Beam Cartridge");//needs texture
+  event.create("explode_cartridge").displayName("Explosive Cartridge");//needs texture
+  event.create("fire_cartridge").displayName("Fire Cartridge");//needs texture
+  event.create("teleport_cartridge").displayName("Teleport Cartridge");//needs texture
+  event.create("illumination_cartridge").displayName("Light Cartridge");//needs texture
+  event.create("uncured_void_cartridge").displayName("Uncured Void Cartridge");//needs texture
+  event.create("void_cartridge").displayName("Void Cartridge");//needs texture
 
 
 });
