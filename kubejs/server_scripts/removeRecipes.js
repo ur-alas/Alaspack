01b99639b418904e065db605b755bf49c98c8b3a
ur-alas <al+github2@drye.ga>
2025-11-18 14:18:23 -0800

    today's progress

--- kubejs/server_scripts/removeRecipes.js
+++ kubejs/server_scripts/removeRecipes.js
@@ -2,4 +2,5 @@ ServerEvents.recipes((event) => {
   event.remove({ mod: "yttr" }); //remove yttr recipes
   //restore yttrium pressure plate recipes
   event.remove({ output: "#yttr:antler_potion" }); //not working for some godforsaken reason
+  event.remove({ mod: "createvintageimprovements" }); //remove create vintage additions recipes
 })
